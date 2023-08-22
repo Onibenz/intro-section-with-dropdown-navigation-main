7e82ed2795f739d60312ebfd0ca151a2abb1bdef
Oni Victor <onibenz@protonmail.com>
2023-08-22 19:07:26 +0100

    Javascript edit

--- index.js
+++ index.js
@@ -12,9 +12,30 @@ function closeMenu() {
     document.querySelector("body").classList.remove('nav-overlay');
 }
 
-document.querySelector("summary")
-        .addEventListener('click', switchArrow);
-function switchArrow() {
+let feature = document.querySelector(".feature");
+let featureMenu = document.querySelector(".feature-menu");
+feature.addEventListener("click", featureArrow);
+function featureArrow() {featureMenu.classList.toggle("open");
+    if(featureMenu.classList.contains("open")) {
     document.querySelector(".arrow-down").style.display = 'none';
     document.querySelector(".arrow-up").style.display = 'block';
+}
+else {
+    document.querySelector(".arrow-down").style.display = 'block';
+     document.querySelector(".arrow-up").style.display = 'none';
+}}
+
+let company = document.querySelector(".company");
+let companyMenu = document.querySelector(".company-menu");
+company.addEventListener("click", companyArrow);
+function companyArrow() {
+    companyMenu.classList.toggle("open");
+    if(companyMenu.classList.contains("open")) {
+    document.querySelector(".company-down").style.display = 'none';
+    document.querySelector(".company-up").style.display = 'block';
+}
+else {
+    document.querySelector(".company-down").style.display = 'block';
+     document.querySelector(".company-up").style.display = 'none';
+}
 }
